23689ba3c30bf1d0b7fd466b8af19225125790bf
Piero Narciso <pieronarciso988@gmail.com>
2023-05-07 18:27:46 -0500

    new challenges

---
+++ tuple-to-object/tuple-to-object.ts
@@ -0,0 +1,5 @@
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type TupleToObject<T extends readonly any[]> = {[P in T[number]]: P}
+
+type result = TupleToObject<typeof tuple> // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
