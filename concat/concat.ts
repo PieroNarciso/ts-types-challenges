8148eccb743a0b24670aa65755c9e5d8c8b5c04c
Piero Narciso <pieronarciso988@gmail.com>
2023-05-07 19:50:41 -0500

    concat challenge

---
+++ concat/concat.ts
@@ -0,0 +1,13 @@
+type Concat<T extends any[], U extends any[]> = [...T, ...U]
+
+type Result = Concat<[1], [2]> // expected to be [1, 2]
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+]
