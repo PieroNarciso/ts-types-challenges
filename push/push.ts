818fddda6a1e7ce5a8343016ad5ab3728cc7c24e
Piero Narciso <pieronarciso988@gmail.com>
2023-05-07 20:50:28 -0500

    push challenge

---
+++ push/push.ts
@@ -0,0 +1,12 @@
+type Push<T extends any[], U> = [...T, U]
+
+type Result = Push<[1, 2], '3'> // [1, 2, '3']
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
